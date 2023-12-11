e48657af1d2cf91eaf1197aa495e75e2c4067d14
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2023-12-11 20:12:26 +0200

    Fix release error

--- tests/integration/test_main_url.py
+++ tests/integration/test_main_url.py
@@ -4,7 +4,7 @@ import requests
 
 HEROKU_PR_NUMBER = os.getenv("HEROKU_PR_NUMBER")
 
-def get_main():
-    1 / 0
+
+def test_get_main():
     r = requests.get(f"https://hilel-review-pr-{HEROKU_PR_NUMBER}.herokuapp.com")
     assert r.ok
